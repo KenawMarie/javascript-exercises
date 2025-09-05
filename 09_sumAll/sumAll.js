092e491556140b3b2219076e41722f3e5d7c2164
kenaw marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-05 09:18:42 -0900

    finish the ninth exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+function sumAll(a,b){
+let array;
+  if(a>=0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b)){
+        if(a<b){
+             array=Array.from({length:b-a+1},()=>a++).reduce((acc,cur)=> acc + cur,0)
+        }
+        else if(a>b){
+            array=Array.from({length:a-b+1},()=>b++).reduce((acc,cur)=> acc + cur,0)
+        }
+    }
+    else{
+        return 'ERROR'
+    }
+   return array
+ }
 
 // Do not edit below this line
 module.exports = sumAll;
