376d7f842842a8526da7970091b54a46b3cf95cb
kenaw marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-05 10:03:03 -0900

    finish the eleventh exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Tf) {
+    let Tc=5*(Tf-32)/9
+    return Number.isInteger(Tc)? Tc :Number(Tc.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Tc) {
+  let Tf= (9*Tc)/5 +32
+  return Number.isInteger(Tf)? Tf :Number(Tf.toFixed(1))
 };
 
 // Do not edit below this line
