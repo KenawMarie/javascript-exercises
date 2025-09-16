9dcf526ca9f1ae1769c1b10508802c8a1fc0d128
Kenaw Marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-16 15:13:53 +0900

    finish the thirteenth exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+     let string= a.replaceAll(' ','').replaceAll('!','').replaceAll(',','').replaceAll('.','')
+     let arr=string.split('').map((e)=>e.toLowerCase())
+     let reverse=arr.slice().reverse()
+     let array=[]
+     for(let i=0;i<arr.length;i++){
+         if(arr[i]===reverse[i]){
+              array.push(true)
+          }
+         else{
+              array.push(false)
+         }
+      }
+   if(array.includes(false)){
+     return false
+    }
+   else{
+     return true
+    }
 
 // Do not edit below this line
 module.exports = palindromes;
