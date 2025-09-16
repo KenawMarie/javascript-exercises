0d34b2160b3cd8da491f46e3b3de6fd66cc150fd
Kenaw Marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-16 15:58:17 +0900

    refactor and fix some bugs

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,9 +1,21 @@
 const fibonacci = function(n) {
   let arr=[1,1];
-  for(let i=0; i<n;i++){
+ let m= Number(n) 
+  if(m<0){
+    return 'OOPS'
+    }
+  else if(m===1){
+    return 1
+  }
+  else if(m===0){
+    return 0
+  }
+  for(let i=0; i<m;i++){
     if(i>1)
     arr.push(arr[i-1]+arr[i-2])
+   
   }
+  
   return arr.at(-1)
 };
 
