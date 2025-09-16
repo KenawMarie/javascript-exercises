07a043d3579070e6a9d025d0200f127642befb73
Kenaw Marie <204995551+KenawMarie@users.noreply.github.com>
2025-09-16 15:41:27 +0900

    finish the fourteenth exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let arr=[1,1];
+  for(let i=0; i<n;i++){
+    if(i>1)
+    arr.push(arr[i-1]+arr[i-2])
+  }
+  return arr.at(-1)
 };
 
 // Do not edit below this line
